0a40d6083b618507da612fdd8d05e3cf5cfa0034
unknown <himanshugarg58063@gmail.com>
2024-10-11 00:06:46 +0530

    impletment auth , appwrite and seentry

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,17 +1,18 @@
 import HeaderBox from '@/components/HeaderBox'
 import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
+import { getLoggedInUser } from '@/lib/actions/user.actions'
 import React from 'react'
 
-const Home = () => {
-    const loggedIn = { firstName: 'Himanshu', lastName: 'Garg', email: 'himanshu@gmail.com' }
+const Home = async () => {
+    const loggedIn = await getLoggedInUser()
     return (
         <section className='home'>
             <div className="home-content">
                 <header className='home-header'>
                     <HeaderBox type="greeting"
                         title="Welcome"
-                        user={loggedIn?.firstName || 'Guest'}
+                        user={loggedIn?.name || 'Guest'}
                         subtext="Access and manage your account and transactions efficiently."
                     />
                     <TotalBalanceBox
--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -14,6 +14,7 @@ import { sidebarLinks } from '@/constants'
 import Link from 'next/link'
 import { cn } from '@/lib/utils'
 import { usePathname } from 'next/navigation'
+import Footer from './Footer'
 
 const MobileNav = ({ user }: MobileNavProps) => {
     const pathname = usePathname()
@@ -62,7 +63,7 @@ const MobileNav = ({ user }: MobileNavProps) => {
                                 USER
                             </nav>
                         </SheetClose>
-                        FOOTER
+                        <Footer user={user} type="mobile" />
                     </div>
                 </SheetContent>
             </Sheet>
